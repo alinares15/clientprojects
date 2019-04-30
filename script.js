b9aff7737d93b0ab0dfc0aac99221dcd4d0e0f8d
Ashley Linares <alinares.u@rrghst.org>
2019-04-30 22:20:02 +0000

    javascript function

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-$("search-song").click(function(){
+$("#searchSong").click(function(){
+console.log("hello")
     $.ajax({
-        url: `https://api.soundcloud.com/tracks?q=beyonce&client_id=5aa8e389ba4e24b6106af5159ab3e344`,
+        url: `https://api.soundcloud.com/tracks?q=${$('#inputfield').val()}&client_id=5aa8e389ba4e24b6106af5159ab3e344`,
         method: "GET",
         success: function(response) {
-            console.log(response);
-            var link = response.file;
-            $('.imageContainer').html("<img src='" + link + "'/>");
+            console.log(response[0].title);
+            $('#imageContainer').empty();
+            response.forEach(function(songData){
+                $('#imageContainer').append(`<h1>${songData.title}</h1>`);
+                $('#imageContainer').append(`<img src='${songData.user.avatar_url}'>`);
+                $('#imageContainer').append(`<p>${songData.genre}</p>`);
+                $('#imageContainer').append(`<p>${songData.likes_count}</p>`);
+
+            });
 
         },
     });
